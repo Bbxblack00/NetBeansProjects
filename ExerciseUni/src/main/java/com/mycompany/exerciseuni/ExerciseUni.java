c798f8d95f16235aee599e310b41c5eea78a88b6
Benny <benny@pop-os.localdomain>
2023-10-28 00:06:01 +0200

    First commit.

---
+++ ExerciseUni/src/main/java/com/mycompany/exerciseuni/ExerciseUni.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exerciseuni;
+import com.mycompany.exerciseuni.stampaaschermo;
+
+/**
+ *
+ * @author benny
+ */
+public class ExerciseUni {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        stampaaschermo ciao = new stampaaschermo();
+        ciao.stampaaschermo();
+    }
+}
---
+++ ExerciseUni/src/main/java/com/mycompany/exerciseuni/stampaaschermo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exerciseuni;
+
+/**
+ *
+ * @author benny
+ */
+public class stampaaschermo {
+    public void stampaaschermo(){
+        System.out.println("ciao mondo");
+        
+    }
+}
